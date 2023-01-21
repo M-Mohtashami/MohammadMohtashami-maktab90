aaf8aa64356c1dc8a7191008260042029eafb2dc
m-mohtashami <m.mohtashami88@gmail.com>
2023-01-22 00:20:39 +0330

    convert yes and no function to arrow function

--- js/HW5-5.js
+++ js/HW5-5.js
@@ -5,10 +5,10 @@ const ask = (question, yes, no) => {
 
 ask(
   "Do you agree?",
-  function () {
+  () => {
     alert("You agreed.");
   },
-  function () {
+  () => {
     alert("You canceled the execution.");
   }
 );
